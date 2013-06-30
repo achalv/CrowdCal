9e3f4f405c7242f7c38760745e8ae37aa5b48d99
Danish Mughal <mughal.danish@gmail.com>
2013-06-30 02:41:18 -0700

    simple fb authentication

---
+++ CrowdCal/app/models/user.rb
@@ -0,0 +1,49 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  provider               :string(255)
+#  uid                    :string(255)
+#  name                   :string(255)
+#
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+	devise :omniauthable, :omniauth_providers => [:facebook]
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :name
+
+  # Devise method for setting up a new user in the system
+	def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+	  user = User.where(:provider => auth.provider, :uid => auth.uid).first
+	  unless user
+	    user = User.create(name:auth.extra.raw_info.name,
+	                         provider:auth.provider,
+	                         uid:auth.uid,
+	                         email:auth.info.email,
+	                         password:Devise.friendly_token[0,20]
+	                         )
+	  end
+	  user
+	end
+
+end
--- CrowdCal/config/routes.rb
+++ CrowdCal/config/routes.rb
@@ -1,5 +1,7 @@
 CrowdCal::Application.routes.draw do
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   root to: 'main#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
