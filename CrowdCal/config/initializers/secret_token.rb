a9ef456a839c2e455b479b2e2c3f519cb51296f5
Danish Mughal <mughal.danish@gmail.com>
2013-06-30 02:12:21 -0700

    boilerplate initial

---
+++ CrowdCal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdCal::Application.config.secret_token = '7ac30b2677ebb49444bbe6d07687c1a56f251897204196443bc47c50990e78e3f711d78a83ffc8fae1627e0324debc57603181a77ee393d0e15f7a79cab20f9e'
